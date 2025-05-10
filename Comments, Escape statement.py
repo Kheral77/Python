7c116e7bb823029de453d4808ca20b9a05e3f16d
Abishek Acharya <117247831+Kheral77@users.noreply.github.com>
2025-05-10 16:27:21 +0545

    Add files via upload

---
+++ Comments, Escape statement.py
@@ -0,0 +1,11 @@
+
+print("hello welcome to my website")
+# # good habit 
+# # I am learning python
+# I am doing my best
+# (ctrl + /)
+
+
+
+# # Escape statement
+print("how are you\n doing \nnew line")
